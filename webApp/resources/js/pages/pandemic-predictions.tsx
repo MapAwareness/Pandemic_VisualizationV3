ff9c7de3fa1d9fff36bdedcb863199b080b8524a
Kévin Lebeau <kevin.lebeau-pro@outlook.fr>
2025-07-02 15:23:08 +0200

    Modif Gitignore

--- webApp/resources/js/pages/pandemic-predictions.tsx
+++ webApp/resources/js/pages/pandemic-predictions.tsx
@@ -147,8 +147,8 @@ export default function PandemicPredictions() {
                                 onChange={e => setDisease(e.target.value)}
                                 className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
                             >
-                                <option value="corona">Corona</option>
-                                <option value="variole">Variole</option>
+                                <option value="corona">Covid-19</option>
+                                <option value="variole">Variole du singe</option>
                             </select>
                         </div>
                         <div>
